ab52ed67681cd57ef1279058a74d3f14ca52547c
MakeIt2021 <boolean0707@gmail.com>
2023-01-28 22:06:16 +0300

    Improvements...

--- index.js
+++ index.js
@@ -101,30 +101,58 @@ try {
         // water_message_id = (await ctx.reply('Сегодня Вы выпили $ стаканов')).message_id
         await ctx.scene.enter('water')
     })
+} catch (e) {
+    ctx.reply('Ой, что-то пошло не так. Можно всё починить, нажав сюда: /start')
+}
+try {
     bot.action('sleep', async ctx => {
         ctx.deleteMessage()
         await ctx.scene.enter('sleep')
     })
+} catch (e) {
+    ctx.reply('Ой, что-то пошло не так. Можно всё починить, нажав сюда: /start')
+}
+try {
     bot.action('meals', async ctx => {
         ctx.deleteMessage()
         await ctx.scene.enter('meals')
     })
+} catch (e) {
+    ctx.reply('Ой, что-то пошло не так. Можно всё починить, нажав сюда: /start')
+}
+try {
     bot.action('sport', async ctx => {
         ctx.deleteMessage()
         await ctx.scene.enter('sport')
     })
-    bot.action('stress', async ctx => {
+} catch (e) {
+    ctx.reply('Ой, что-то пошло не так. Можно всё починить, нажав сюда: /start')
+}
+try{
+        bot.action('stress', async ctx => {
         ctx.deleteMessage()
         await ctx.scene.enter('stress')
     })
+} catch (e) {
+    ctx.reply('Ой, что-то пошло не так. Можно всё починить, нажав сюда: /start')
+}
+try {
     bot.action('back', async ctx => {
         ctx.deleteMessage()
         await ctx.scene.enter('mainMenu')
     })
+} catch (e) {
+    ctx.reply('Ой, что-то пошло не так. Можно всё починить, нажав сюда: /start')
+}
+try {
     bot.action('plus', async ctx => {
         ctx.reply('Ой, кажется эта кнопка была отправлена довольно давно. Рекомендуем Вам воспользоваться командой /start')
     })
-    bot.action('minus', async ctx => {
+} catch (e) {
+    ctx.reply('Ой, что-то пошло не так. Можно всё починить, нажав сюда: /start')
+}
+try {
+        bot.action('minus', async ctx => {
         ctx.reply('Рекомендуем Вам воспользоваться командой /start')
     })
 } catch (e) {
